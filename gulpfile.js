733a8ffde993b5ee3ee1e676e27f44683893d019
Eder Soares <edetec@gmail.com>
2018-02-22 12:51:39 -0300

    fix(test): the version of @ng-select/ng-select was locked to 0.16.0

--- gulpfile.js
+++ gulpfile.js
@@ -383,6 +383,7 @@ gulp.task('rollup-bundle', (cb) => {
       // ATTENTION:
       // Add any other dependency or peer dependency of your library here
       // This is required for UMD bundle users.
+    '@ng-select/ng-select': '@ng-select/ng-select',
     };
     const rollupBaseConfig = {
       name: _.camelCase(config.libraryName),
